757b45a0182b3b32cc20802880eee13387b4d8a9
chen <13439594353@163.com>
2024-04-22 15:11:30 +0800

    更新

--- pages/home/home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<view></view>
-</template>
-
-<script setup>
-const app = getApp() // 当前应用实例
-</script>
-
-<style lang="scss" scoped></style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,15 +2,6 @@
 	<view></view>
 </template>
 
-<script setup>
-const pages = [
-	{
-		path: 'pages/index/index',
-		style: {
-			navigationBarTitleText: 'uni-app',
-		},
-	},
-]
-</script>
+<script setup></script>
 
 <style lang="scss"></style>

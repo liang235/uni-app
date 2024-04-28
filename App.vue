e77be561fb8f3ebe48b7a74aa4831d149104b43d
chen <13439594353@163.com>
2024-04-28 21:02:14 +0800

    更新

--- App.vue
+++ App.vue
@@ -5,6 +5,7 @@ export default {}
 <style lang="scss">
 /*每个页面公共css */
 @import '@/uni_modules/uview-plus/index.scss';
+@import '@/static/scss/uview-plus.scss';
 @import '@/static/scss/uni-app.scss';
 @import '@/static/iconfont/iconfont.css';
 @import '@/static/scss/global.scss';
